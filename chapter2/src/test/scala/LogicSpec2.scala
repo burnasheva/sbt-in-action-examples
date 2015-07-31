f88e0ed8b443f6652238b09b0cc0b8b7bcb4f9d7
Nadezhda Burnasheva <nadezhda.burnasheva@gmail.com>
2015-07-31 09:11:35 +0300

    all tests should pass

--- chapter2/src/test/scala/LogicSpec2.scala
+++ chapter2/src/test/scala/LogicSpec2.scala
@@ -3,12 +3,12 @@ package org.preownedkittens
 import org.specs2.mutable.Specification
 
 object LogicSpec2 extends Specification {
-  skipAllIf(true)
+  //skipAllIf(true)
   "The 'matchLikelihood' method" should {
     "be 100% when all attributes match" in {
       val tabby = Kitten(1, List("male", "tabby"))
       val prefs = BuyerPreferences(List("male", "tabby"))
-      Logic.matchLikelihood(tabby, prefs) must beGreaterThan(1.999)
+      Logic.matchLikelihood(tabby, prefs) must beGreaterThan(0.999)
     }
     "be 0% when no attributes match" in {
       val tabby = Kitten(1, List("male", "tabby"))

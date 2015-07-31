fcbd446c0234d12cf617a0a5ee4211c03293a825
Nadezhda Burnasheva <nadezhda.burnasheva@gmail.com>
2015-07-31 09:15:57 +0300

    all tests should fail

--- chapter2/src/test/scala/LogicSpec.scala
+++ chapter2/src/test/scala/LogicSpec.scala
@@ -8,13 +8,13 @@ object LogicSpec extends Specification {
     "be 100% when all attributes match" in {
       val tabby = Kitten(1, List("male", "tabby"))
       val prefs = BuyerPreferences(List("male", "tabby"))
-      Logic.matchLikelihood(tabby, prefs) must beGreaterThan(0.999)
+      Logic.matchLikelihood(tabby, prefs) must beGreaterThan(1.999)
     }
     "be 0% when no attributes match" in {
       val tabby = Kitten(1, List("male", "tabby"))
       val prefs = BuyerPreferences(List("female", "calico"))
       val result = Logic.matchLikelihood(tabby, prefs) 
-      result must beLessThan(0.001)
+      result must beLessThan(-0.001)
     }
   }
 }

61531b63dc7fc7c685bbff85474acae601815cdf
Nadezhda Burnasheva <nadezhda.burnasheva@gmail.com>
2015-08-01 10:25:47 +0300

    all tests failed now

--- chapter2/src/main/scala/logic.scala
+++ chapter2/src/main/scala/logic.scala
@@ -6,7 +6,7 @@ object Logic {
     val matches = buyer.attributes map { attribute => 
       kitten.attributes contains attribute
     }
-    val nums = matches map { b => if(b) -1.0 else 0.0 }
+    val nums = matches map { b => if(b) -1.0 else 10.0 }
     nums.sum / nums.length
   }
 }
